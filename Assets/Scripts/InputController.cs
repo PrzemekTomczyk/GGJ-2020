4aa3ad0d619d5eded015c3a9bf71f7f49720d68e
Aaron <C00241596@itcarlow.ie>
2020-02-01 20:08:07 +0000

    Added Screens and assets

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 330; i < 349; i++)
+        {
+            // TEST TO GET CONTROLLER INFO
+            //string keycode = "KeyCode.JoystickButton" + i;
+            if (Input.GetKey((KeyCode)i))
+            {
+                print(i.ToString());
+            }
+
+        }
+    }
+
+    void onClick(int playerNumber)
+    {
+
+    }
+}
